b72a6a759ce4340673c476624eecd813e54be001
cesardelabarreda <cesar04_95@hotmail.com>
2016-11-16 12:31:53 -0600

    Se corrigo la Base de Datos

---
+++ inventarioquimica.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-11-2016 a las 19:29:33
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventarioquimica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalleequipo`
+--
+
+CREATE TABLE `detalleequipo` (
+  `cantidad` int(11) NOT NULL,
+  `observaciones` varchar(1000) NOT NULL,
+  `cantidadRegresada` int(11) NOT NULL,
+  `pedidoId` int(11) NOT NULL,
+  `equipoId` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallematerial`
+--
+
+CREATE TABLE `detallematerial` (
+  `cantidad` int(11) NOT NULL,
+  `observaciones` varchar(1000) NOT NULL,
+  `cantidadRegresada` int(11) NOT NULL,
+  `pedidoId` int(11) NOT NULL,
+  `materialId` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallereactivo`
+--
+
+CREATE TABLE `detallereactivo` (
+  `cantidad` int(11) NOT NULL,
+  `observaciones` varchar(1000) NOT NULL,
+  `pedidoId` int(11) NOT NULL,
+  `reactivoId` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `clave` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `marca` varchar(100) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `localizacion` varchar(100) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `laboratorio`
+--
+
+CREATE TABLE `laboratorio` (
+  `clave` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `material`
+--
+
+CREATE TABLE `material` (
+  `clave` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `marca` varchar(100) NOT NULL,
+  `capacidad` varchar(100) NOT NULL,
+  `localizacion` varchar(100) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `profesorId` varchar(100) NOT NULL,
+  `usuarioId` varchar(10) NOT NULL,
+  `laboratorioId` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reactivo`
+--
+
+CREATE TABLE `reactivo` (
+  `clave` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `marca` varchar(100) NOT NULL,
+  `presentacion` varchar(100) NOT NULL,
+  `contenido` varchar(100) NOT NULL,
+  `localizacion` varchar(100) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `matricula` varchar(10) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `rol` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `creadorId` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`clave`);
+
+--
+-- Indices de la tabla `laboratorio`
+--
+ALTER TABLE `laboratorio`
+  ADD PRIMARY KEY (`clave`);
+
+--
+-- Indices de la tabla `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`clave`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reactivo`
+--
+ALTER TABLE `reactivo`
+  ADD PRIMARY KEY (`clave`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`matricula`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
