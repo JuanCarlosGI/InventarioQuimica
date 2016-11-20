cea32c5aa7b83aa934a7bcab316870aa7b888de9
Juan Carlos Guzman <juancarlos.gi94@gmail.com>
2016-11-20 11:27:35 -0600

    Agrego Conumible y DetalleConsumible

---
+++ src/java/models/Consumible.java
@@ -0,0 +1,175 @@
+package models;
+
+/**
+ * Clase representando un consumible del laboratorio.
+ * @author Juan Carlos Guzmán Islas
+ */
+public class Consumible {
+    /**
+     * La clave del consumible.
+     */
+    private String clave;
+
+    /**
+     * El nombre del consumible.
+     */
+    private String nombre;
+
+    /**
+     * La marca del consumible.
+     */
+    private String marca;
+
+    /**
+     * La presentación del consumible.
+     */
+    private String presentacion;
+
+    /**
+     * El contenido del consumible.
+     */
+    private String contenido;
+
+    /**
+     * La localizacion del consumible.
+     */
+    private String localizacion;
+
+    /**
+     * La descripción del consumible.
+     */
+    private String descripcion;
+
+    /**
+     * La cantidad disponible que se tiene.
+     */
+    private int cantidad;
+
+    /**
+     * Getter de clave.
+     * @return La clave.
+     */
+    public final String getClave() {
+        return clave;
+    }
+
+    /**
+     * Setter de clave.
+     * @param nuevaClave La nueva clave.
+     */
+    public final void setClave(final String nuevaClave) {
+        this.clave = nuevaClave;
+    }
+
+    /**
+     * Getter de nombre.
+     * @return El nombre.
+     */
+    public final String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Setter de nombre.
+     * @param nuevoNombre El nuevo nombre.
+     */
+    public final void setNombre(final String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    /**
+     * Getter de marca.
+     * @return La marca.
+     */
+    public final String getMarca() {
+        return marca;
+    }
+
+    /**
+     * Setter de marca.
+     * @param nuevaMarca La nueva marca.
+     */
+    public final void setMarca(final String nuevaMarca) {
+        this.marca = nuevaMarca;
+    }
+
+    /**
+     * Getter de presentacion.
+     * @return La presentacion.
+     */
+    public final String getPresentacion() {
+        return presentacion;
+    }
+
+    /**
+     * Setter de presentacion.
+     * @param nuevaPresentacion La nueva presentacion.
+     */
+    public final void setPresentacion(final String nuevaPresentacion) {
+        this.presentacion = nuevaPresentacion;
+    }
+
+    /**
+     * Getter de contenido.
+     * @return El contenido.
+     */
+    public final String getContenido() {
+        return contenido;
+    }
+
+    /**
+     * Setter de contenido.
+     * @param nuevoContenido El nuevo contenido.
+     */
+    public final void setContenido(final String nuevoContenido) {
+        this.contenido = nuevoContenido;
+    }
+
+    /**
+     * Getter de localizacion.
+     * @return La localizacion.
+     */
+    public final String getLocalizacion() {
+        return localizacion;
+    }
+
+    /**
+     * Setter de localizacion.
+     * @param nuevaLocalizacion La nueva localizacion.
+     */
+    public final void setLocalizacion(final String nuevaLocalizacion) {
+        this.localizacion = nuevaLocalizacion;
+    }
+
+    /**
+     * Getter de descripcion.
+     * @return La descripcion.
+     */
+    public final String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * Setter de descripcion.
+     * @param nuevaDescripcion La nueva descripcion.
+     */
+    public final void setDescripcion(final String nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+
+    /**
+     * Getter de cantidad.
+     * @return La cantidad.
+     */
+    public final int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * Setter de cantidad.
+     * @param nuevaCantidad La nueva cantidad.
+     */
+    public final void setCantidad(final int nuevaCantidad) {
+        this.cantidad = nuevaCantidad;
+    }
+}
---
+++ src/java/models/DetalleConsumible.java
@@ -0,0 +1,90 @@
+package models;
+
+/**
+ * @author Juan Carlos Guzmán Islas
+ */
+public class DetalleConsumible {
+    /**
+     * Cantidad de unidades que se estan pidiendo.
+     */
+    private int cantidad;
+
+    /**
+     * Observaciones sobre el equipo que se pide.
+     */
+    private String observaciones;
+
+    /**
+     * ID del pedido al que el detalle pertenece.
+     */
+    private int pedidoId;
+
+    /**
+     * Clave del material al que se hace referencia.
+     */
+    private String reactivoId;
+
+    /**
+     * Regresa el valor de cantidad.
+     * @return El valor de cantidad.
+     */
+    public final int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * Asigna el valor de cantidad.
+     * @param nuevaCantidad El nuevo valor de cantidad.
+     */
+    public final void setCantidad(final int nuevaCantidad) {
+        this.cantidad = nuevaCantidad;
+    }
+
+    /**
+     * Regresa el valor de observaciones.
+     * @return El valor de observaciones.
+     */
+    public final String getObservaciones() {
+        return observaciones;
+    }
+
+    /**
+     * Asigna el valor de observaciones.
+     * @param nuevasObservaciones El nuevo valor de observaciones.
+     */
+    public final void setObservaciones(final String nuevasObservaciones) {
+        this.observaciones = nuevasObservaciones;
+    }
+
+    /**
+     * Regresa el valor de pedidoId.
+     * @return El valor de pedidoId.
+     */
+    public final int getPedidoId() {
+        return pedidoId;
+    }
+
+    /**
+     * Asigna el valor de pedidoId.
+     * @param nuevoPedidoId El nuevo valor de pedidoID.
+     */
+    public final void setPedidoId(final int nuevoPedidoId) {
+        this.pedidoId = nuevoPedidoId;
+    }
+
+    /**
+     * Regresa el valor de reactivoId.
+     * @return El valor de reactivoId.
+     */
+    public final String getReactivoId() {
+        return reactivoId;
+    }
+
+    /**
+     * Asigna el valor de reactivoId.
+     * @param nuevoReactivoId En nuevo valor de reactivoId.
+     */
+    public final void setReactivoId(final String nuevoReactivoId) {
+        this.reactivoId = nuevoReactivoId;
+    }
+}
