4b2e20a71090c67c373ac6e816d2a01eb353a2a6
Mauro Amarante <mauro.amarante94@gmail.com>
2016-11-20 14:26:05 -0600

    added interactive feature for adding new students to labs

---
+++ web/js/agregarLaboratorio.js
@@ -0,0 +1,20 @@
+function nuevosAlumnos(e) {
+	var elem = document.getElementById("alumnos");
+
+	while (elem.hasChildNodes()) {
+    	elem.removeChild(elem.lastChild);
+	}
+
+	var num = e.value;
+
+	for (var i = 0; i < num; i++) {
+		var input = document.createElement("input");
+		input.required = true;
+		input.placeholder = "A00000000";
+		input.type = "text";
+		input.class = "validate";
+		input.id = "matricula-" + i;
+		input.name = "matricula-" + i;
+		elem.appendChild(input);
+	}
+}
